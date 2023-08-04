bba70cb46780f7228bc0982c8098f14024b7baba
Guilherme Dias <gui.diasz@hotmail.com>
2023-08-04 16:53:15 -0300

    feat: Footer component

---
+++ src/components/Footer/styles/Footer.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.footer`
+  background: black;
+  padding: 3.75rem 2rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  color: rgba(255, 255, 255, 0.7);
+  flex-direction: column;
+  max-width: 1140px;
+  margin: auto;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  padding: 0.75rem 0;
+  flex-wrap: wrap;
+  row-gap: 0.75rem;
+  justify-content: space-between;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  gap: 0.75rem;
+  flex-basis: 33.33%;
+  min-width: 224px;
+`;
+
+export const Link = styled.a`
+  text-decoration: underline;
+  display: inline-flex;
+  &:focus-visible {
+    outline: solid 2px #fff;
+  }
+`;
+
+export const Title = styled.p`
+  padding: 0.75rem 0;
+`;
+
+export const Text = styled.p`
+  padding: 0.75rem 0;
+`;
---
+++ src/containers/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Footer } from '../components/';
+
+const FooterContainer = () => {
+  return (
+    <>
+      <Footer>
+        <Footer.Title>
+          Dúvidas? Ligue <Footer.Link href="#">0800 591 2876</Footer.Link>
+        </Footer.Title>
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Link href="#">Perguntas frequentes</Footer.Link>
+            <Footer.Link href="#">Media Center</Footer.Link>
+            <Footer.Link href="#">Resgatar cartão pré-pago</Footer.Link>
+            <Footer.Link href="#">Termos de Uso</Footer.Link>
+            <Footer.Link href="#">Informações corporativas</Footer.Link>
+            <Footer.Link href="#">Avisos legais</Footer.Link>
+          </Footer.Column>
+          <Footer.Column>
+            <Footer.Link href="#">Central de Ajuda</Footer.Link>
+            <Footer.Link href="#">Relações com investidores</Footer.Link>
+            <Footer.Link href="#">Comprar cartão pré-pago</Footer.Link>
+            <Footer.Link href="#">Privacidade</Footer.Link>
+            <Footer.Link href="#">Entre em contato</Footer.Link>
+            <Footer.Link href="#">Só na Netflix</Footer.Link>
+          </Footer.Column>
+          <Footer.Column>
+            <Footer.Link href="#">Conta</Footer.Link>
+            <Footer.Link href="#">Carreiras</Footer.Link>
+            <Footer.Link href="#">Formas de assistir</Footer.Link>
+            <Footer.Link href="#">Preferências de cookies</Footer.Link>
+            <Footer.Link href="#">Teste de velocidade</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+
+        <Footer.Text>Netflix Brasil</Footer.Text>
+      </Footer>
+    </>
+  );
+};
+
+export default FooterContainer;
