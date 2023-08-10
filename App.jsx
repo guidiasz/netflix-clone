7d77d6e215997406e0e6f24b687bebb882e794c2
Guilherme Dias <gui.diasz@hotmail.com>
2023-08-10 16:06:26 -0300

    feat: add routes constant

--- App.jsx
+++ App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './src/pages/Home';
+import * as ROUTES from './src/constants/routes';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="signin" element={<p>signin</p>} />
-        <Route path="signup" element={<p>signup</p>} />
-        <Route path="browse" element={<p>browse</p>} />
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.SIGN_IN} element={<p>signin</p>} />
+        <Route path={ROUTES.SIGN_UP} element={<p>signup</p>} />
+        <Route path={ROUTES.BROWSE} element={<p>browse</p>} />
       </Routes>
     </BrowserRouter>
   );
